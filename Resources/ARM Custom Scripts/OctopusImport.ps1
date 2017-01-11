227e9a327d8899cb63bf61c23df37bd600d66bf6
Paul Marston <paul@marston.me>
2017-01-11 08:15:06 +0000

    Support for displaying custom script log output

--- Resources/ARM Custom Scripts/OctopusImport.ps1
+++ Resources/ARM Custom Scripts/OctopusImport.ps1
@@ -1,7 +1,11 @@
 if (!(Test-Path 'C:\CustomScriptLogs')) { New-Item -ItemType Directory -Path 'C:\CustomScriptLogs' | Out-Null }
 
-Update-DscConfiguration -Wait -Verbose *>> 'C:\CustomScriptLogs\DSC-Update.log'
+$logFile = 'C:\CustomScriptLogs\{0}.log' -f ([datetime]::UtcNow.tostring('o').Replace(':','.').Substring(0,19))
+Update-DscConfiguration -Wait -Verbose *>> $logFile
 
-& net use O: \\#{StackresourcesName}.file.core.windows.net\octopusdeploy /u:#{StackResourcesName} #{StackResourcesKey} *>> 'C:\CustomScriptLogs\Octopus-Import.log'
-& "C:\Program Files\Octopus Deploy\Octopus\Octopus.Migrator.exe" import --console --directory="O:\" --password=#{StackAdminPassword} --overwrite *>> 'C:\CustomScriptLogs\Octopus-Import.log'
-& net use O: /DELETE *>> 'C:\CustomScriptLogs\Octopus-Import.log'
+& net use O: \\#{StackresourcesName}.file.core.windows.net\octopusdeploy /u:#{StackResourcesName} #{StackResourcesKey} *>> $logFile
+& "C:\Program Files\Octopus Deploy\Octopus\Octopus.Migrator.exe" import --console --directory="O:\" --password=#{StackAdminPassword} --overwrite *>> $logFile
+
+Copy-Item -Path $logFile -Destination O:\CustomScript.log -Force *>> $logFile
+
+& net use O: /DELETE *>> $logFile
