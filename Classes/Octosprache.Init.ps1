3ad1ab6d2879f127265498169f9e3552b5708d2b
Paul Marston <paul@marston.me>
2017-01-13 10:33:13 +0000

    back to what worked

--- Classes/Octosprache.Init.ps1
+++ Classes/Octosprache.Init.ps1
@@ -1,6 +1,5 @@
 function Register-NuGetAssembly {
-    param($PackageId, $Version, $Framework, $Assembly, $TestType)
-    if (([System.Management.Automation.PSTypeName]$TestType).Type) { return }
+    param($PackageId, $Version, $Framework, $Assembly)
     $tempFolder = Join-Path $script:TempPath 'Octosprache'
     $assemblyPath = Join-Path $tempFolder "lib\$Framework\$Assembly.dll"
     if (!(Test-Path $assemblyPath)) {
@@ -20,7 +19,7 @@ $tempFolder = Join-Path $script:TempPath 'Octosprache'
 if (!(Test-Path $tempFolder)) {
     New-Item -Path $tempFolder -ItemType Directory | Out-Null
 }
-Register-NuGetAssembly 'Newtonsoft.Json' '9.0.1' 'net40'          'Newtonsoft.Json' 'Newtonsoft.Json.JsonConvert'
-Register-NuGetAssembly 'Sprache'         '2.1.0' 'net40'          'Sprache'         'Sprache.Parse' 
-Register-NuGetAssembly 'Octostache'      '2.0.7' 'net40'          'Octostache'      'Octostache.VariableDictionary'
-Register-NuGetAssembly 'Humanizer.Core'  '2.1.0' 'netstandard1.0' 'Humanizer'       'Humanizer.TimeSpanHumanizeExtensions'
+Register-NuGetAssembly 'Newtonsoft.Json' '9.0.1' 'net40'          'Newtonsoft.Json'
+Register-NuGetAssembly 'Sprache'         '2.1.0' 'net40'          'Sprache'        
+Register-NuGetAssembly 'Octostache'      '2.0.7' 'net40'          'Octostache'     
+Register-NuGetAssembly 'Humanizer.Core'  '2.1.0' 'netstandard1.0' 'Humanizer'    
