c81a68af49a92c168a73fdc10679b74c75902d00
Paul Marston <paul@marston.me>
2017-01-14 21:11:39 +0000

    switching to webclient as it is many times faster than iwr

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -4,24 +4,22 @@ param(
 )
 
 if ($PSVersionTable.PSVersion.Major -lt 5) {
-    Write-Error 'AutomationStack requires PowerShell 5 to start the rest of the process, this is the only non-automated step. Go to ''Download WMF 5.0'' from https://msdn.microsoft.com/en-us/powershell'
+    Write-Error 'AutomationStack requires PowerShell 5 to begin. Go to ''Download WMF 5.0'' at https://msdn.microsoft.com/en-us/powershell'
     return
 }
 
 if (Test-Path $Path) {
-    Write-Warning 'Previous AutomationStack working directory exists, cleaning it up...'
+    Write-Warning 'Previous AutomationStack directory exists, removing it...'
     Remove-Item $Path -Recurse -Force
 }
 
-Write-Output 'Downloading AutomationStack archive from GitHub...'
-$download = Invoke-WebRequest -Verbose -UseBasicParsing -Uri "https://github.com/$GitHubAccount/AutomationStack/archive/master.zip"
 $tempFile = [System.IO.Path]::ChangeExtension((New-TemporaryFile).FullName, 'zip')
-Write-Output "Saving file to ${tempFile}"
-Set-Content -Path $tempFile -Value $download.Content -Force -Encoding Byte
+Write-Output "Downloading AutomationStack archive from GitHub to $tempFile..."
+(New-Object System.Net.WebClient).DownloadFile("https://github.com/$GitHubAccount/AutomationStack/archive/master.zip", $tempFile)
+
 Write-Output 'Extracting archive...'
-# This cmdlet requires PowerShell 5
 Expand-Archive -Path $tempFile -DestinationPath $Path -Force
-Move-Item -Path (Join-Path $Path 'AutomationStack-master\*') -Destination $Path
+Move-Item -Path (Join-Path $Path 'AutomationStack-master\*') -Destination $Path -Force
 
 $moduleManifest = Join-Path $Path 'AutomationStack.psd1'
 if (!(Test-Path $moduleManifest)) {
@@ -29,6 +27,6 @@ if (!(Test-Path $moduleManifest)) {
     return
 }
 
-Write-Output 'AutomationStack repo aquired, importing module & starting new deployment'
+Write-Output 'AutomationStack aquired, importing module & starting deployment...'
 Import-Module $moduleManifest -Force
-New-AutomationStack
+AutomationStack\New-AutomationStack
