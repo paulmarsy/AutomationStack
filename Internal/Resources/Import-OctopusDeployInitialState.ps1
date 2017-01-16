d79f614acbc56d2d5b1905e097a6e001c7e600ab
Paul Marston <paul@marston.me>
2017-01-16 20:13:24 +0000

    bringing back custom script extension logs in real time

--- Internal/Resources/Import-OctopusDeployInitialState.ps1
+++ Internal/Resources/Import-OctopusDeployInitialState.ps1
@@ -1,11 +1,33 @@
 function Import-OctopusDeployInitialState {
     Write-Host 'Initialising Azure VM Custom Script Extension...'
-    Set-AzureRmVMCustomScriptExtension -ResourceGroupName $CurrentContext.Get('OctopusRg') -Location $CurrentContext.Get('AzureRegion') -VMName $CurrentContext.Get('OctopusVMName') -Name "OctopusImport" -StorageAccountName $CurrentContext.Get('StackResourcesName') -StorageAccountKey $CurrentContext.Get('StackResourcesKey')  -FileName "OctopusImport.ps1" -ContainerName "scripts" -Run 'OctopusImport.ps1' | Out-Null
 
-    $logFilePath =Join-Path $TempPath 'CustomScript.log'
-    $stackresources = New-AzureStorageContext -StorageAccountName $CurrentContext.Get('StackResourcesName') -StorageAccountKey $CurrentContext.Get('StackResourcesKey')
-  
-    Get-AzureStorageFileContent -ShareName octopusdeploy -Path 'CustomScript.log' -Destination $logFilePath -Force -Context $stackresources
-    Get-Content -Path $logFilePath | % { Write-Host "[Custom Script Log] $_" }
-    Remove-AzureStorageFile  -ShareName octopusdeploy -Path 'CustomScript.log'  -Context $stackresources 
+    try {
+        & net use O: \\$CurrentContext.Get('StackResourcesName').file.core.windows.net\octopusdeploy /persistent:no /u:$CurrentContext.Get('StackResourcesName') $CurrentContext.Get('StackResourcesKey')
+        $logFile = 'O:\CustomScript.log'        
+        $logPosition = 0
+        New-Item -Path $logFile -ItemType File | Out-Null
+
+        $azureprofile = [System.IO.Path]::GetTempFileName()
+        Save-AzureRmProfile -Path $azureprofile -Force
+
+        $job = Start-Job -ScriptBlock {
+            param($SubscriptionId, $AzureProfile, $ResourceGroupName, $Location, $VMName, $Name, $StorageAccountName, $StorageAccountKey, $FileName, $ContainerName, $Run)
+            Select-AzureRmProfile -Profile $AzureProfile
+            Select-AzureRmSubscription  -SubscriptionId $SubscriptionId
+
+            Set-AzureRmVMCustomScriptExtension -ResourceGroupName $ResourceGroupName -Location $Location -VMName $VMName -Name $Name -StorageAccountName $StorageAccountName -StorageAccountKey $StorageAccountKey  -FileName $FileName -ContainerName $ContainerName -Run $Run
+            Remove-Item -Path $AzureProfile -Force | Out-Null
+        } -ArgumentList @((Get-AzureRmContext).Subscription.SubscriptionId, $azureprofile, $CurrentContext.Get('OctopusRg'), $CurrentContext.Get('AzureRegion'), $CurrentContext.Get('OctopusVMName'), "OctopusImport", $CurrentContext.Get('StackResourcesName'), $CurrentContext.Get('StackResourcesKey'), "OctopusImport.ps1", "scripts", 'OctopusImport.ps1')
+
+        while ($job.State -eq 'Running') {
+            Start-Sleep 5
+            Get-Content -Path $logFile | Select-Object -Skip $logPosition | % { $logPosition++; $_ | Out-Host }
+        }
+        Write-Host
+        Write-Host ('Custom Script Extension finished with state: {0} after {1}' -f $job.State, ([Humanizer.TimeSpanHumanizeExtensions]::Humanize($job.PSEndTime - $job.PSBeginTime, 2)))
+        Remove-Item -Path $logFile -Force
+    }
+    finally {
+        & net use O: /DELETE /Y
+    }
 }
--- Resources/ARM Custom Scripts/OctopusImport.ps1
+++ Resources/ARM Custom Scripts/OctopusImport.ps1
@@ -1,28 +1,33 @@
 if (!(Test-Path 'C:\CustomScriptLogs')) { New-Item -ItemType Directory -Path 'C:\CustomScriptLogs' | Out-Null }
 
-$logFile = 'C:\CustomScriptLogs\{0}.log' -f ([datetime]::UtcNow.tostring('o').Replace(':','.').Substring(0,19))
-(Get-Date).ToString() | Tee-Object -FilePath $logFile -Append
-
-$lcm = Get-DscLocalConfigurationManager
-while ($lcm.LCMState -ne 'Idle') {
-    "DSC Local Configuration Manager state is $($lcm.LCMState); waiting 10 seconds..." | Tee-Object -FilePath $logFile -Append
-    Start-Sleep -Seconds 10
-    $lcm = Get-DscLocalConfigurationManager
+$localLogFile = 'C:\CustomScriptLogs\{0}.log' -f ([datetime]::UtcNow.tostring('o').Replace(':','.').Substring(0,19))
+$remoteLogFile = 'O:\CustomScript.log'
+filter Write-Log {
+    $entry = '[{0}]{1}: {2}' -f $env:COMPUTERNAME, (Get-Date).ToShortTimeString(), ($_ | Out-String)
+    Add-Content -Path $localLogFile -Value $entry
+    Add-Content -Path $remoteLogFile -Value $entry
 }
-"{0}Starting DSC{0}" -f ("-"*40) | Tee-Object -FilePath $logFile -Append
-Update-DscConfiguration -Verbose -Wait *>&1 | Tee-Object -FilePath $logFile -Append
-Start-DscConfiguration -UseExisting -Wait -Verbose *>&1 | Tee-Object -FilePath $logFile -Append
 
-
-
-"{0}Starting Octopus Import{0}" -f ("-"*40) | Tee-Object -FilePath $logFile -Append
 try {
-    & net use O: \\#{StackresourcesName}.file.core.windows.net\octopusdeploy /u:#{StackResourcesName} #{StackResourcesKey} *>&1 | Tee-Object -FilePath $logFile -Append
+    & net use O: \\#{StackResourcesName}.file.core.windows.net\octopusdeploy /u:#{StackResourcesName} #{StackResourcesKey} *>&1 | Write-Log
 
-    & "C:\Program Files\Octopus Deploy\Octopus\Octopus.Migrator.exe" import --console --directory="O:\" --password=#{StackAdminPassword} --overwrite *>&1 | Tee-Object -FilePath $logFile -Append
+    (Get-Date).ToString() | Write-Log
 
-    Copy-Item -Path $logFile -Destination O:\CustomScript.log -Force *>&1 | Tee-Object -FilePath $logFile -Append
+    "{0}Waiting for Local Configuration Manager{0}" -f ("-"*40) | Write-Log
+    $lcm = Get-DscLocalConfigurationManager
+    while ($lcm.LCMState -ne 'Idle') {
+        "DSC Local Configuration Manager state is $($lcm.LCMState); waiting..." | Write-Log
+        Start-Sleep -Seconds 10
+        $lcm = Get-DscLocalConfigurationManager
+    }
+
+    "{0}Starting DSC{0}" -f ("-"*40) | Write-Log
+    Update-DscConfiguration -Verbose -Wait *>&1 | Write-Log
+    Start-DscConfiguration -UseExisting -Wait -Verbose *>&1 | Write-Log
+
+    "{0}Starting Octopus Import{0}" -f ("-"*40) | Write-Log
+    & "C:\Program Files\Octopus Deploy\Octopus\Octopus.Migrator.exe" import --console --directory="O:\" --password=#{StackAdminPassword} --overwrite *>&1 | Write-Log
 }
 finally {
-    & net use O: /DELETE *>&1 | Tee-Object -FilePath $logFile -Append
+    & net use O: /DELETE *>&1 | Write-Log
 }
