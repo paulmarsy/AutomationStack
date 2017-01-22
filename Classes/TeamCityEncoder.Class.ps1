41d018717bf2bcccf3575368911d1d2a77a578eb
Paul Marston <paul@marston.me>
2017-01-22 21:20:00 +0000

    decode teamcity secure parameters

--- Classes/TeamCityEncoder.Class.ps1
+++ Classes/TeamCityEncoder.Class.ps1
@@ -1,8 +1,34 @@
 class TeamCityEncoder {
     TeamCityEncoder([Octosprache]$Octosprache) {
-        $this.Octosprache = $Octosprache
+     $this.Octosprache = $Octosprache
     }
     hidden $Octosprache
+    hidden static $Prefix = 'zxx'
+  
+    hidden static [byte[]] GetUnsignedKey() {
+        [sbyte[]]$SignedKey = @(61, 22, 11, 57, 110, 89, -20, -1, 0, 99, 111, -120, 55, 4, -9, 10, 11, 45, 71, -89, 21, -99, 54, 51)
+        [byte[]]$unsignedKey = New-Object byte[] $SignedKey.Length
+        [System.Buffer]::BlockCopy($SignedKey, 0, $unsignedKey, 0, $SignedKey.Length)
+        return $unsignedKey 
+    }
+    [string] Unscramble([string] $Scrambled) {
+        if (-not $Scrambled.StartsWith([TeamCityEncoder]::Prefix) -or (($Scrambled.Length-[TeamCityEncoder]::Prefix.Length) % 2) -ne 0) {
+            throw "$Scrambled is not a valid scrambled string"
+        }
+        $scrambledWithoutPrefix = $Scrambled.Substring([TeamCityEncoder]::Prefix.Length, $Scrambled.Length-[TeamCityEncoder]::Prefix.Length)
+        $encodedBytes = for ($i = 0; $i -lt $scrambledWithoutPrefix.Length; $i += 2) {
+            [System.Convert]::ToByte($scrambledWithoutPrefix.Substring($i, 2), 16)
+        }
+
+        $csp = New-Object System.Security.Cryptography.TripleDESCryptoServiceProvider
+        $csp.Mode = [System.Security.Cryptography.CipherMode]::ECB
+        $csp.Key = [TeamCityEncoder]::GetUnsignedKey()
+        $decryptor = $csp.CreateDecryptor()
+
+        $decodedBytes = $decryptor.TransformFinalBlock($encodedBytes, 0, $encodedBytes.Count)
+
+        return [System.Text.Encoding]::UTF8.GetString($decodedBytes)
+    }
 
     Hash([string]$Key, [string]$Value) {
         $saltBytes = New-Object byte[] 32
