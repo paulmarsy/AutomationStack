5ee17a4367585ea5d1d5c111eeb5c95301f14eae
Paul Marston <paul@marston.me>
2017-01-25 10:51:32 +0000

    fix

--- Internal/Infrastructure/Initialize-KeyVault.ps1
+++ Internal/Infrastructure/Initialize-KeyVault.ps1
@@ -8,11 +8,12 @@ function Initialize-KeyVault {
     $CurrentContext.Set('KeyVaultResourceId', $keyvaultdeploy.keyVaultId.Value)
     $CurrentContext.Set('KeyVaultUri', $keyvaultdeploy.vaultUri.Value)
 
-    $account = Get-AzureRmContext | % Account
-    if ($account.AccountType -eq 'User') {
-        Write-Host 'Granting current authenticated user full permissions to KeyVault'
-        $azureAdUserObject = Get-AzureRmADUser -UserPrincipalName $account.Id
-        Set-AzureRmKeyVaultAccessPolicy -VaultName $CurrentContext.Get('KeyVaultName') -ObjectId $azureAdUserObject.Id.Guid -PermissionsToKeys all -PermissionsToSecrets all -ResourceGroupName $CurrentContext.Get('InfraRg')
+    Get-AzureRmContext | % Account | ? AccountType -eq 'User' | % {
+        $azureAdUserObject = Get-AzureRmADUser -UserPrincipalName $_.Id =ErrorAction Ignore
+        if ($azureAdUserObject) {
+            Write-Host 'Granting authenticated user full permissions to KeyVault'
+            Set-AzureRmKeyVaultAccessPolicy -VaultName $CurrentContext.Get('KeyVaultName') -ObjectId $azureAdUserObject.Id.Guid -PermissionsToKeys all -PermissionsToSecrets all -ResourceGroupName $CurrentContext.Get('InfraRg')
+        }
     }
 
     Set-AzureRmKeyVaultAccessPolicy -VaultName $CurrentContext.Get('KeyVaultName') -ResourceGroupName $CurrentContext.Get('InfraRg') -EnabledForTemplateDeployment -EnabledForDiskEncryption 
