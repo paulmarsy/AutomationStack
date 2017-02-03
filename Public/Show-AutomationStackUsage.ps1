106b2b81f3d8a7b23258a7056f4f3ceb7da26b3c
Paul Marston <paul@marston.me>
2017-02-03 18:49:06 +0000

    wip

--- Public/Show-AutomationStackUsage.ps1
+++ Public/Show-AutomationStackUsage.ps1
@@ -9,61 +9,6 @@ function Show-AutomationStackUsage {
         [switch]$NoFormat
     )
 
-    function Get-BillingPeriod {
-        param($StartDateTime, $RateCard)
-        
-        $usage = Invoke-AzureRestApi -ResourceId '/providers/Microsoft.Commerce/UsageAggregates' -WithoutSubscriptionId `
-                    -ODataQuery ('&reportedStartTime={0}&reportedEndTime={1}&aggregationGranularity=Hourly&showDetails=true' -f `
-                    $StartDateTime.ToString('yyyy-MM-ddTHH:00:00Z'),
-                    $StartDateTime.AddHours(1).ToString('yyyy-MM-ddTHH:00:00Z'))
-
-        if ($usage.error) {
-            Write-Warning $usage.error
-            return
-        }
-        $usage | 
-            % value |
-            % properties |
-            ? { $null -ne $_.instanceData } |
-            ? {
-                $data = $_.instanceData | ConvertFrom-Json | % Microsoft.Resources
-                $data.tags.application -eq 'AutomationStack' -and $data.tags.udp -eq $UDP
-            }  |
-            % {
-                $rate = $ratecard.Meters | ? meterId -eq $_.meterId | % meterRates
-                $price = $_.quantity * $rate.0
-                New-Object psobject -Property @{ 
-                    Uri = $data.resourceUri
-                    Quantity = $_.quantity
-                    Price = $price
-                    Name = $_.meterName
-                    Category = $_.meterCategory
-                    SubCategory = $_.meterSubcategory
-                }
-            } |
-            Group-Object -Property Uri,Name | 
-            % {
-                $resourcePrice = 0
-                $resourceQuantity = 0
-                $_.Group | % {
-                    $resourcePrice += $_.Price
-                    $resourceQuantity += $_.Quantity
-                }
-                if ([string]::IsNullOrWhitespace($_.Group[0].SubCategory) -or $_.Group[0].Category -eq 'Virtual Machines') {
-                    $category = $_.Group[0].Category
-                } else {
-                    $category = '{0} - {1}' -f $_.Group[0].Category, $_.Group[0].SubCategory
-                }
-                New-Object psobject -Property @{ 
-                    Uri = $_.Group[0].Uri
-                    Price = $resourcePrice
-                    Quantity = $resourceQuantity
-                    Name = $_.Group[0].Name
-                    Category = $category
-                }
-            }
-    }
-
     if ($null -ne $CurrentContext -and $null -eq $UDP) {
         $UDP  = $CurrentContext.Get('UDP')
     }
@@ -79,46 +24,71 @@ function Show-AutomationStackUsage {
                     $Locale,
                     $RegionInfo)
     Write-Host -ForegroundColor DarkGreen 'done'  
-    Write-Host  
 
-    $billingData = @()
-    while ($HoursToCollect -gt 0) {
-        $billingPeriod = $billingPeriod.AddHours(-1)
-        Write-Host -NoNewLine -ForegroundColor Yellow ('Getting billing period {0}... ' -f $billingPeriod.ToString('yyyy-MM-dd HH'))
-        $billingData += Get-BillingPeriod -StartDateTime $billingPeriod -RateCard $rateCard
-        Write-Host -ForegroundColor DarkGreen 'done'
-        $HoursToCollect--
-    }
-    Write-Host
-
-    Write-Host -NoNewLine -ForegroundColor Yellow 'Merging billing data... '
+    Write-Host -NoNewLine -ForegroundColor Yellow 'Getting billing data... '
     $total = 0
-    $mergedBillingData = $billingData | Group-Object -Property Uri,Name  | % { 
-        $resourcePrice = 0
-        $resourceQuantity = 0
-        $_.Group | % {
-            $resourcePrice += $_.Price
-            $resourceQuantity += $_.Quantity
-        }
-        $total += $resourcePrice
-        New-Object psobject -Property @{ 
-            ResourceName = [Microsoft.Azure.Commands.ResourceManager.Cmdlets.Components.ResourceIdUtility]::GetResourceName($_.Group[0].Uri)
-            Price = '{0:C}' -f $resourcePrice
-            Quantity = [System.Math]::Round($resourceQuantity, 3)
-            Name = $_.Group[0].Name
-            Category = $_.Group[0].Name
-        }
+    $usage = Invoke-AzureRestApi -ResourceId '/providers/Microsoft.Commerce/UsageAggregates' -WithoutSubscriptionId `
+                -ODataQuery ('&reportedStartTime={0}&reportedEndTime={1}&aggregationGranularity=Hourly&showDetails=true' -f `
+                $StartDateTime.ToString('yyyy-MM-ddTHH:00:00Z'),
+                $StartDateTime.AddHours(1).ToString('yyyy-MM-ddTHH:00:00Z'))
+
+    if ($usage.error) {
+        Write-Warning $usage.error
+        return
     }
+
+    $billingData = $usage | 
+        % value |
+        % properties |
+        ? { $null -ne $_.instanceData } |
+        ? {
+            $data = $_.instanceData | ConvertFrom-Json | % Microsoft.Resources
+            $data.tags.application -eq 'AutomationStack' -and $data.tags.udp -eq $UDP
+        }  |
+        % {
+            $rate = $ratecard.Meters | ? meterId -eq $_.meterId | % meterRates
+            $price = $_.quantity * $rate.0
+            New-Object psobject -Property @{ 
+                Uri = $data.resourceUri
+                Quantity = $_.quantity
+                Price = $price
+                Name = $_.meterName
+                Category = $_.meterCategory
+                SubCategory = $_.meterSubcategory
+            }
+        } |
+        Group-Object -Property Uri,Name | 
+        % {
+            $resourcePrice = 0
+            $resourceQuantity = 0
+            $_.Group | % {
+                $resourcePrice += $_.Price
+                $resourceQuantity += $_.Quantity
+            }
+            $total += $resourcePrice
+            if ([string]::IsNullOrWhitespace($_.Group[0].SubCategory) -or $_.Group[0].Category -eq 'Virtual Machines') {
+                $category = $_.Group[0].Category
+            } else {
+                $category = '{0} - {1}' -f $_.Group[0].Category, $_.Group[0].SubCategory
+            }
+            New-Object psobject -Property @{ 
+                ResourceName = [Microsoft.Azure.Commands.ResourceManager.Cmdlets.Components.ResourceIdUtility]::GetResourceName($_.Group[0].Uri)
+                Price = '{0:C}' -f $resourcePrice
+                Quantity = [System.Math]::Round($resourceQuantity, 3)
+                Name = $_.Group[0].Name
+                Category = $_.Group[0].Name
+            }
+        }
     Write-Host -ForegroundColor DarkGreen 'done'  
     Write-Host  
    
     if ($NoFormat) {
         return @{
             TotalPrice = '{0:C}' -f $total
-            Items = $mergedBillingData
+            Items = $billingData
          }
      } else {
-         $mergedBillingData |
+         $billingData |
             Sort-Object -Property Price,ResourceName,Category,Name -Descending |
             Format-Table ResourceName,Category,Name,Quantity,Price -AutoSize
 
