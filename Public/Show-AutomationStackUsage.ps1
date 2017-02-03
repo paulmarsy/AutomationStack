cac2701ed7e3d27a802d54b3c4a2ae2dfcf9b656
Paul Marston <paul@marston.me>
2017-02-03 20:14:28 +0000

    fixing usage

--- Public/Show-AutomationStackUsage.ps1
+++ Public/Show-AutomationStackUsage.ps1
@@ -1,7 +1,7 @@
 function Show-AutomationStackUsage {
     param(
         $UDP,
-        $HoursToCollect = 24,
+        $BillingPeriodDuration = 7,
         $AzureOfferNumber = '0063P',
         $Currency = 'GBP',
         $Locale = 'en-GP',
@@ -13,9 +13,6 @@ function Show-AutomationStackUsage {
         $UDP  = $CurrentContext.Get('UDP')
     }
 
-    $now = Get-Date
-    $billingPeriod = (Get-Date -Year $now.Year -Month $now.Month -Day $now.Day -Hour $now.Hour -Minute 0 -Second 0 -Millisecond 0).AddHours(-1)
-
     Write-Host -NoNewLine -ForegroundColor Yellow 'Getting Azure Billing RateCard... '
     $rateCard = Invoke-AzureRestApi -ResourceId '/providers/Microsoft.Commerce/RateCard' -WithoutSubscriptionId `
                 -ODataQuery ("`$filter=OfferDurableId eq 'MS-AZR-{0}' and Currency eq '{1}' and Locale eq '{2}' and RegionInfo eq '{3}'" -f `
@@ -25,18 +22,22 @@ function Show-AutomationStackUsage {
                     $RegionInfo)
     Write-Host -ForegroundColor DarkGreen 'done'  
 
-    Write-Host -NoNewLine -ForegroundColor Yellow 'Getting billing data... '
-    $total = 0
+    Write-Host -NoNewLine -ForegroundColor Yellow 'Downloading Azure Usage Aggregates... '
+    $now = Get-Date
+    $billingPeriodEnd = (Get-Date -Year $now.Year -Month $now.Month -Day $now.Day -Hour $now.Hour -Minute 0 -Second 0 -Millisecond 0).AddHours(-1)
+    $billingPeriodStart = $billingPeriodEnd.AddDays($BillingPeriodDuration * -1)
     $usage = Invoke-AzureRestApi -ResourceId '/providers/Microsoft.Commerce/UsageAggregates' -WithoutSubscriptionId `
                 -ODataQuery ('&reportedStartTime={0}&reportedEndTime={1}&aggregationGranularity=Hourly&showDetails=true' -f `
-                $StartDateTime.ToString('yyyy-MM-ddTHH:00:00Z'),
-                $StartDateTime.AddHours(1).ToString('yyyy-MM-ddTHH:00:00Z'))
+                $billingPeriodStart.ToString('yyyy-MM-ddTHH:00:00Z'),
+                $billingPeriodEnd.ToString('yyyy-MM-ddTHH:00:00Z'))
+    Write-Host -ForegroundColor DarkGreen 'done'  
 
     if ($usage.error) {
         Write-Warning $usage.error
         return
     }
 
+    $total = 0
     $billingData = $usage | 
         % value |
         % properties |
@@ -76,14 +77,13 @@ function Show-AutomationStackUsage {
                 Price = '{0:C}' -f $resourcePrice
                 Quantity = [System.Math]::Round($resourceQuantity, 3)
                 Name = $_.Group[0].Name
-                Category = $_.Group[0].Name
+                Category = $category
             }
         }
     Write-Host -ForegroundColor DarkGreen 'done'  
-    Write-Host  
    
     if ($NoFormat) {
-        return @{
+        @{
             TotalPrice = '{0:C}' -f $total
             Items = $billingData
          }
