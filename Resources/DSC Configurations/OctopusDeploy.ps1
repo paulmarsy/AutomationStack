13abe9674a88168b038a59553aea671e5b0f392e
Paul Marston <paul@marston.me>
2017-02-16 21:21:57 +0000

    parameter name

--- Resources/DSC Configurations/OctopusDeploy.ps1
+++ Resources/DSC Configurations/OctopusDeploy.ps1
@@ -102,19 +102,19 @@ Configuration OctopusDeploy
                 $hostHeader = 'http://{0}/' -f $using:OctopusHostName
                 $octopusServerExe = Join-Path $env:ProgramFiles 'Octopus Deploy\Octopus\Octopus.Server.exe'
 
-                & $octopusServerExe create-instance --console --instance OctopusServer --config "$($env:SystemDrive)\Octopus\OctopusServer.config" *>&1 | Tee-Object -Append -Path $using:octopusConfigLogFile
+                & $octopusServerExe create-instance --console --instance OctopusServer --config "$($env:SystemDrive)\Octopus\OctopusServer.config" *>&1 | Tee-Object -Append -FilePath $using:octopusConfigLogFile
                 if ($LASTEXITCODE -ne 0) { throw "Exit code $LASTEXITCODE from Octopus Server: create-instance" }
-                & $octopusServerExe configure --console --instance OctopusServer --home "C:\Octopus" --storageConnectionString $using:OctopusConnectionString --upgradeCheck "True" --upgradeCheckWithStatistics "True" --webAuthenticationMode "UsernamePassword" --webForceSSL "False" --webListenPrefixes $hostHeader --commsListenPort "10943" --serverNodeName $using:OctopusNodeName *>&1 | Tee-Object -Append -Path $using:octopusConfigLogFile
+                & $octopusServerExe configure --console --instance OctopusServer --home "C:\Octopus" --storageConnectionString $using:OctopusConnectionString --upgradeCheck "True" --upgradeCheckWithStatistics "True" --webAuthenticationMode "UsernamePassword" --webForceSSL "False" --webListenPrefixes $hostHeader --commsListenPort "10943" --serverNodeName $using:OctopusNodeName *>&1 | Tee-Object -Append -FilePath $using:octopusConfigLogFile
                 if ($LASTEXITCODE -ne 0) { throw "Exit code $LASTEXITCODE from Octopus Server: configure" }
-                & $octopusServerExe database --console --instance OctopusServer --create *>&1 | Tee-Object -Append -Path $using:octopusConfigLogFile
+                & $octopusServerExe database --console --instance OctopusServer --create *>&1 | Tee-Object -Append -FilePath $using:octopusConfigLogFile
                 if ($LASTEXITCODE -ne 0) { throw "Exit code $LASTEXITCODE from Octopus Server: database" }
                 
                 $response = Invoke-WebRequest -UseBasicParsing -Uri "https://octopusdeploy.com/api/licenses/trial" -Method POST -Body @{ FullName=$env:USERNAME; Organization=$env:USERDOMAIN; EmailAddress="${env:USERNAME}@${env:USERDOMAIN}.onmicrosoft.com"; Source="azure" } -Verbose
                 $licenseBase64 = [System.Convert]::ToBase64String(((New-Object System.Text.UTF8Encoding($false)).GetBytes($response.Content)))
-                & $octopusServerExe license --console --instance OctopusServer --licenseBase64 $licenseBase64 *>&1 | Tee-Object -Append -Path $using:octopusConfigLogFile
+                & $octopusServerExe license --console --instance OctopusServer --licenseBase64 $licenseBase64 *>&1 | Tee-Object -Append -FilePath $using:octopusConfigLogFile
                 if ($LASTEXITCODE -ne 0) { throw "Exit code $LASTEXITCODE from Octopus Server: license" }
 
-                & $octopusServerExe service --console --instance OctopusServer --install --reconfigure --stop *>&1 | Tee-Object -Append -Path $using:octopusConfigLogFile
+                & $octopusServerExe service --console --instance OctopusServer --install --reconfigure --stop *>&1 | Tee-Object -Append -FilePath $using:octopusConfigLogFile
                 if ($LASTEXITCODE -ne 0) { throw "Exit code $LASTEXITCODE from Octopus Server: service" }
 
                 [System.IO.FIle]::WriteAllText($using:octopusConfigStateFile, $LASTEXITCODE,[System.Text.Encoding]::ASCII)
